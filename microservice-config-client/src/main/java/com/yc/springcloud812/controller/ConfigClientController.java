b73494a4ac63a10c650262558b1322a645523cb2
19537 <1953745128@qq.com>
2021-09-09 20:07:04 +0800

    detailcloud

---
+++ microservice-config-client/src/main/java/com/yc/springcloud812/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.yc.springcloud812.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServers;
+
+    @RequestMapping("/config")
+    public String getConfig() {
+        return "ApplicationName = " + this.applicationName + "、EurekaServers = "
+                + this.eurekaServers;
+    }
+}
